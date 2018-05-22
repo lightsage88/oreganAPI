a8af77c917527398876611856eee8b731e5ac767
Adrian Rosales <adrian.e.rosales@gmail.com>
2018-05-22 10:52:12 -0700

    I have integrated USPS, FEDEX, and DHL into our shipping program. Now we need to set up the callback functions to play with our front end and allow us to have some kind of call and response play so we can present customers with shipping choices based on what they enter in terms of shipping address

--- server.js
+++ server.js
@@ -82,50 +82,116 @@ function closeServer(){
 		});
 	});
 }
+// shippo_test_c3f2fe6e2f03cd8c8908a44c6509cd0a679b345b
+var shippo = require('shippo')('shippo_test_c3f2fe6e2f03cd8c8908a44c6509cd0a679b345b');
+let shipment;
 
-var shippo = require('shippo')(SHIPPO_KEY);
-
-var addressFrom = {
-	"name":"Shawn Toppple",
-	"street1": "215 Clayton St.",
-	"city":"San Francisco",
-	"state":"CA",
-	"zip":"94117",
-	"country":"US",
-	"phone":"+1 555 341 9393",
-	"email":"shippottle@goshippo.com"
+var addressFrom  = {
+    "name": "Oregan-PTD",
+    "street1": "1785 SW Pheasant Drive",
+    "city": "Aloha",
+    "state": "OR",
+    "zip": "97006",
+    "country": "US",
+    "phone": "+1 971-226-2846",
+    "email": "adrian.e.rosales@gmail.com"
 };
 
 var addressTo = {
-	"name":"Mr. Hippo",
-	"street1":"Broadway 1",
-	"city":"New York",
-	"state":"NY",
-	"zip":"10007",
-	"country":"US",
-	"phone":"+1 555 341 9393",
-	"email":"mrhippo@goshippo.com"
+    "name": "Mr Hippo",
+    "street1": "30 Bạch Đằng",
+    "city": "Kon Tum",
+    "country": "VN",
+    "zip": "580000",
+    "phone": "+84 260 3863 334",
+    "email": "mrhippo@goshippo.com"
 };
 
 var parcel = {
-	// "length": "",
-	// "width":"",
-	// "height":"",
-	// "distance_unit":"",
-	"template": "FedEx_Box_Large_1",
-	"weight":"2",
-	"mass_unit": "lb"
+    "length": "5",
+    "width": "5",
+    "height": "5",
+    "distance_unit": "in",
+    "weight": "2",
+    "mass_unit": "lb"
 };
+//we specify which carrier accounts we want to use by passing in each
+// shippo.carrieraccount.create({
+//     "carrier":"fedex", 
+//     "account_id":"510087240", 
+//     "parameters":{"meter":"119044507"},
+//     "test":true,
+//     "active":true
+// }, function(err, account) {
+//   	console.log(account);
+// });
+
 
 shippo.shipment.create({
-	"address_from":addressFrom, 
-	"address_to":addressTo,
-	"parcels": [parcel],
-	"async": false
-}, function(err,shipment){
-	//asynchronously called
-	console.log(shipment);
-}); 
+    "address_from": addressFrom,
+    "address_to": addressTo,
+    "parcels": [parcel],
+    "async": false
+}, function(err, shipment){
+    console.log(shipment);
+    shipment=shipment;
+    var rate = shipment.rates[0];
+    console.log(rate);
+    console.log('take it back to the register');
+    console.log(shipment.rates);
+	shippo.transaction.create({
+		"rate": rate.object_id,
+		"label_file_type": "PDF",
+		"async": false
+	}, function(err, transaction){
+		console.log(transaction);
+	});
+});
+// var addressFrom = {
+// 	"name":"Shawn Toppple",
+// 	"street1": "215 Clayton St.",
+// 	"city":"San Francisco",
+// 	"state":"CA",
+// 	"zip":"94117",
+// 	"country":"US",
+// 	"phone":"+1 555 341 9393",
+// 	"email":"shippottle@goshippo.com"
+// };
+
+// var addressTo = {
+// 	"name":"Mr. Hippo",
+// 	"street1":"Broadway 1",
+// 	"city":"New York",
+// 	"state":"NY",
+// 	"zip":"10007",
+// 	"country":"US",
+// 	"phone":"+1 555 341 9393",
+// 	"email":"mrhippo@goshippo.com"
+// };
+
+// var parcel = {
+// 	// "length": "",
+// 	// "width":"",
+// 	// "height":"",
+// 	// "distance_unit":"",
+// //We should create an algorithm to figure out the combined dimensions of the users cart
+// //then we can make sure that it is smaller than what the various templates offer ;)
+// //also we should have kilograms (kg) be our weight method of choice.
+// 	"template": "FedEx_Box_Large_1",
+// 	"weight":"2",
+// 	"mass_unit": "lb"
+// };
+
+// shippo.shipment.create({
+// 	"address_from":addressFrom, 
+// 	"address_to":addressTo,
+// 	"parcels": [parcel],
+// 	"async": false
+// }, function(err,shipment){
+// 	//asynchronously called
+// 	console.log(shipment);
+// 	shipment= shipment;
+// }); 
 //For the various values, such as TO FROM, WEIGHT, etc,
 //we should make an endpoint for once this portion is
 //modularized, allowing us to make a call to the endpoint and having the
@@ -133,7 +199,15 @@ shippo.shipment.create({
 //Now we have our shipment object showing up in the console.
 //next we need a transaction object
 
+// var rate = shipment.rates[0];
 
+// shippo.transaction.create({
+// 	"rate": rate.object._id,
+// 	"label_file_type": "PDF",
+// 	"async": false
+// }, function(err, transaction){
+// 	console.log(transaction);
+// });
 
 if(require.main === module) {
 	runServer().catch(err => console.error(err));
